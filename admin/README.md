ecd9d9de23088de3bddd9939c565e66e4b925f6d
ritikmaurya4747 <ritikmaurya4747@gmail.com>
2024-09-26 21:32:41 +0530

    order ui and data fetching complete

--- admin/README.md
+++ admin/README.md
@@ -8,6 +8,29 @@
 # creating Add, List, Orders inside pages folder 
 # creating routes in App.jsx file for this pages
 # Sidebar.jsx importing {NavLink} to link this pages  to add some styles after clicking 
+<<<<<<< Tabnine <<<<<<<
+/**undefined+
+ * A functional component that renders the Orders section of an application.undefined+
+ * It fetches and displays order data from a specified URL.undefined+
+ *undefined+
+ * @function Ordersundefined+
+ * @param {Object} props - The component's properties.undefined+
+ * @param {string} props.url - The URL from which to fetch order data.undefined+
+ * @returns {JSX.Element} - The rendered Orders section.undefined+
+ */undefined+
+import React from 'react'undefined+
+import './Orders.css'undefined+
+undefined+
+function Orders({url}) {undefined+
+  return (undefined+
+    <div className='orders'>undefined+
+undefined+
+    </div>undefined+
+  )undefined+
+}undefined+
+undefined+
+export default Ordersundefined+
+>>>>>>> Tabnine >>>>>>>undefined {"conversationId":"dc638b74-1eb5-4e9e-82d8-f90c2e731966","source":"instruct"}
 # creating pages compo UI and some logics add, cart, orders.
 # upload images handle ke liye usestate ka use karenge
 # adding form data information store manage with state and onchangehandler funtion 
@@ -17,3 +40,4 @@
 # removing item from db and cloudinary code in foodController.jsx and List.jsx
 # adding all products using admin panel in databse
 # adding url in app.jsx and pass the url to the compo as a props .
+# adding api in orders.jsx to get data from db to show in orders.jsx ui
--- admin/src/pages/Orders/Orders.jsx
+++ admin/src/pages/Orders/Orders.jsx
@@ -1,9 +1,73 @@
-import React from 'react'
-import './Orders.css'
-function Orders({url}) {
+import React, { useEffect, useState } from "react";
+import "./Orders.css";
+import axios from "axios";
+import { toast } from "react-toastify";
+import { assets } from "../../assets/assets";
+
+function Orders({ url }) {
+  const [orders, setOrders] = useState([]);
+
+  const fetchAllOrders = async (req, res, next) => {
+    const response = await axios.get(url + "/api/order/list");
+    if (response.data.success) {
+      setOrders(response.data.data);
+    } else {
+      toast.error("Error fetching orders");
+    }
+  };
+
+  useEffect(() => {
+    fetchAllOrders();
+  }, []);
+
   return (
-    <div>Orders</div>
-  )
+    <div className="order add">
+      <h3>Order Page</h3>
+      <div className="order-list">
+        {orders.map((order, index) => {
+          return (
+            <div key={index} className="order-item">
+              <img src={assets.parcel_icon} alt="" />
+              <div>
+                <p className="order-item-food">
+                  {order.items.map((item, i) => (
+                    <span key={i}>
+                      {item.name} X {item.quantity}
+                      {i !== order.items.length - 1 && ", "}
+                    </span>
+                  ))}
+                </p>
+                <p className="order-item-name">
+                  {order.address.firstName + " " + order.address.lastName}
+                </p>
+                <div className="order-item-address">
+                  <p>{order.address.street + ","}</p>
+                  <p>
+                    {order.address.city +
+                      ", " +
+                      order.address.state +
+                      ", " +
+                      order.address.country +
+                      ", " +
+                      order.address.pincode}
+                  </p>
+                </div>
+                <p className="order-item-phone">{order.address.phone}</p>
+              </div>
+              
+              <p>Items : {order.items.length}</p>
+              <p>${order.amount}</p>
+              <select>
+                <option value="Food Processing">Food Processing</option>
+                <option value="Out for delivery">Out for delivery</option>
+                <option value="Delivered">Delivered</option>
+              </select>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
 
-export default Orders
+export default Orders;
