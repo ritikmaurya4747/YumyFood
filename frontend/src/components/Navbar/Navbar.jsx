9017024ef3383b8f333b6150ead16092e0d22cfe
ritikmaurya4747 <ritikmaurya4747@gmail.com>
2024-09-26 20:16:38 +0530

    navbar ui caps

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -19,10 +19,10 @@ function Navbar({setShowLogin}) {
     <div className='navbar'>
        <Link to='/'><h1>YumyFood</h1></Link>
        <ul className="navbar-menu">
-        <Link to='/' onClick={()=>setMenu("home")} className={menu === "home"?"active":""}>home</Link>
-        <a href='#explore-menu' onClick={()=>setMenu("menu")} className={menu === "menu"?"active":""}>menu</a>
-        <a href='#app-download' onClick={()=>setMenu("mobile-app")} className={menu === "mobile-app"?"active":""}>mobile-app</a>
-        <a href='#footer' onClick={()=>setMenu("contact us")} className={menu === "contact us"?"active":""}>contact us</a>
+        <Link to='/' onClick={()=>setMenu("home")} className={menu === "home"?"active":""}>Home</Link>
+        <a href='#explore-menu' onClick={()=>setMenu("menu")} className={menu === "menu"?"active":""}>Menu</a>
+        <a href='#app-download' onClick={()=>setMenu("mobile-app")} className={menu === "mobile-app"?"active":""}>Mobile-app</a>
+        <a href='#footer' onClick={()=>setMenu("contact us")} className={menu === "contact us"?"active":""}>Contact us</a>
        </ul>
        <div className="navbar-right">
         <img src={assets.search_icon} alt="" />
