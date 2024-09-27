267e29658d344d1346256dbdfdf2c472abe68a67
ritikmaurya4747 <ritikmaurya4747@gmail.com>
2024-09-27 12:04:54 +0530

    adding backend url

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -10,7 +10,7 @@ import 'react-toastify/dist/ReactToastify.css';
 
 
 function App() {
-  const url = "http://localhost:4000"
+  const url = "https://yumyfood.onrender.com"
   return (
     <div>
     <Navbar/>
