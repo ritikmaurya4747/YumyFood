82f1bf266ea711726107d01fe825f566a9352d4a
ritikmaurya4747 <ritikmaurya4747@gmail.com>
2024-09-27 12:00:08 +0530

    added render live backend url

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -4,8 +4,10 @@ import { createContext, useEffect, useState } from "react";
 export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
+
+  const url = "https://yumyfood.onrender.com"
+  
   const [cartItems, setCartItems] = useState({});
-  const url = "http://localhost:4000"
   const [token,setToken] = useState("");
   const [food_list, setFoodList] = useState([])
 
